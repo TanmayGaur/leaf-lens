83466217052c89a67001bc157117f3aa791e0b43
TanmayGaur <125302283+TanmayGaur@users.noreply.github.com>
2025-03-07 12:21:03 +0530

    Update input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+const Input = React.forwardRef<HTMLInputElement,  React.InputHTMLAttributes<HTMLInputElement>>(({ className, type, ...props }, ref) => {
   return (
     <input
       type={type}
